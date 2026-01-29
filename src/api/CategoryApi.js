557c938c4e451825480cccf0e8941d3b8cd010d2
ouedraogoissaka00956 <oissaka439@gmail.com>
2026-01-29 20:05:01 +0000

    Fix categoryApi import & update frontend

--- src/api/CategoryApi.js
+++ src/api/CategoryApi.js
@@ -1,11 +1,11 @@
 // ============================================
-// src/api/categoryApi.js
+// src/api/ CategoryApi.js
 // ============================================
 
 // Import the configured axios instance
 import api from './axios';
 
-export const categoryApi = {
+export const  CategoryApi = {
   getAll: () => api.get('/categories'),
   create: (data) => api.post('/categories', data),
   update: (id, data) => api.put(`/categories/${id}`, data),
